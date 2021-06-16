915c41f7f64dc30c2d59cb38cd20595c12664a1e
Bea Avis <bea.avis.p@gmail.com>
2021-06-16 11:34:35 +0200

    Updated search component with onSubmit method with a redirect to a search page

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-bar',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchBarComponent implements OnInit {
 
-  constructor() { }
+  // The router will be used to redirect the app to the search page onSubmit.
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm){
+    this.router.navigate(['search', form.value.search]);
   }
 
 }
